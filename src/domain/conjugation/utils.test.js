2946561a34f23d641b88c4edbc502859e1e375c6
fabien_renaud <frd.frenaud@gmail.com>
2021-05-09 20:16:33 +0200

    100% test coverage

--- src/domain/conjugation/utils.test.js
+++ src/domain/conjugation/utils.test.js
@@ -1,20 +1,43 @@
-import {isVerb} from './utils';
-
+import {isVerb, decomposeVerb, composeJamos} from './utils';
 
 describe('isVerb', () => {
-    test('recognize 하다 as a verb', () => {
-        expect(isVerb('하다')).toBe(true);
+    const dataset = ['하다', '먹다', '마시다'];
+    dataset.map((verb) => {
+        test(`${verb} is a verb`, () => {
+            expect(isVerb(verb)).toBe(true);
+        });
     });
 
-    test('does not recognize 안녕 as a verb', () => {
-        expect(isVerb('안녕')).toBe(false);
+    const wrongDataset = ['안녕하세요', '다현', '담', 'coucou', '', undefined];
+    wrongDataset.map((verb) => {
+        test(`${verb} is not a verb`, () => {
+            expect(isVerb(verb)).toBe(false);
+        });
     });
+});
 
-    test('does not recognize 다현 as a verb', () => {
-        expect(isVerb('다현')).toBe(false);
+describe('decomposeVerb', () => {
+    const dataset = [
+        {verb: '하다', expects: {verbStem: ['ㅎ', 'ㅏ'], lastJamo: {ending: '하', hasFinal: false}}},
+        {verb: '보다', expects: {verbStem: ['ㅂ', 'ㅗ'], lastJamo: {ending: 'ㅗ', hasFinal: false}}},
+        {verb: '먹다', expects: {verbStem: ['ㅁ', 'ㅓ', 'ㄱ'], lastJamo: {ending: 'ㅓ', hasFinal: true}}}
+    ];
+    dataset.map(({verb, expects}) => {
+        test(`${verb} equals ${expects}`, () => {
+            expect(decomposeVerb(verb)).toEqual(expects);
+        });
     });
+});
 
-    test('does not recognize 담 as a verb', () => {
-        expect(isVerb('담')).toBe(false);
+describe('composeJamos', () => {
+    const dataset = [
+        {jamosArray: ['ㅁ', 'ㅓ', 'ㄱ', 'ㅅ', 'ㅡ', 'ㅂ', '니다'], expects: '먹습니다'},
+        {jamosArray: ['ㅂ', 'ㅗ', 'ㅏ', '요'], expects: '봐요'},
+        {jamosArray: ['보', 'ㅏ', '요'], expects: '봐요'}
+    ];
+    dataset.map(({jamosArray, expects}) => {
+        test(`${jamosArray} returns ${expects}`, () => {
+            expect(composeJamos(...jamosArray)).toBe(expects);
+        });
     });
 });
